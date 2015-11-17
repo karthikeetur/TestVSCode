b0395695b74d07cf80eb9f5089ef6ee5e772d465
v-kaeetu <karthik.eetur@gmail.com>
2015-11-17 18:56:05 +0530

    1st commit by karthik

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-//add a comment from gopikrishna_adasd
+//add a comment from gopikrishna_updated by karthik
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname1, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
